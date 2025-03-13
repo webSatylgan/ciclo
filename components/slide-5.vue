a45e94e066f3a4b6d9e916a4ffc34f4a2069b695
= <=>
2025-03-13 17:09:59 +0500

    feat: slide-5

---
+++ components/slide-5.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+import SlideLayout from "@/components/ui/slideLayout.vue";
+import TitleLayout from "@/components/ui/titleLayout.vue";
+</script>
+
+<template>
+    <slide-layout html-class="slide-5">
+        <div class="slide-5__text">
+            <title-layout />
+            <p>Dramatically maximize principle-centered
+                models through focused materials. Assertively
+                build fully researched best practices via go
+                forward "outside the box" thinking.</p>
+        </div>
+        <div class="slide-5__imgs">
+            <img src="@/assets/imgs/slide-5-1.jpg" alt="">
+            <img src="@/assets/imgs/slide-5-2.jpg" alt="">
+        </div>
+    </slide-layout>
+</template>
+
+<style scoped>
+
+    ::v-deep(.slide-5__container) {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        gap: 25px;
+        width: 100%;
+    }
+
+    .slide-5__text {
+        max-width: 550px;
+        width: 100%;
+    }
+
+    .slide-5__text p {
+        margin-top: 130px;
+    }
+
+    .slide-5__imgs {
+        max-width: 720px;
+        height: 100vh;
+    }
+
+    .slide-5__imgs img {
+        width: 100%;
+        height: 50%;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,14 @@
     import Slide2 from "@/components/slide-2.vue";
     import Slide3 from "@/components/slide-3.vue";
     import Slide4 from "@/components/slide-4.vue";
+    import Slide5 from "@/components/slide-5.vue";
 
     const siteModules = [
         {name: "hero", component: Hero},
         {name: "slide-2", component: Slide2},
         {name: "slide-3", component: Slide3},
         {name: "slide-4", component: Slide4},
+        {name: "slide-5", component: Slide5},
     ]
 </script>
 
