b500f5252fa52b3f482de5880daa32ad464287a4
= <=>
2025-03-13 14:42:35 +0500

    feat: slide laypout

---
+++ components/ui/slideLayout.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const props = defineProps({
+        htmlClass: String,
+    });
+</script>
+
+<template>
+    <div :class="htmlClass">
+        <div class="container">
+            <div :class="htmlClass + '__container'">
+                <slot>
+
+                </slot>
+            </div>
+        </div>
+    </div>
+</template>
