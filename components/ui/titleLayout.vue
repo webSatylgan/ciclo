d7135d1da24a6957c2492cfb2d2e8ad7eaacc8e2
= <=>
2025-03-13 15:31:05 +0500

    feat: title Layout

---
+++ components/ui/titleLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+    const props = defineProps({
+        title: {
+           type: String,
+           default: "The simplest things are often the truest…",
+        },
+
+        subTitle: {
+            type: String,
+            default: "Multipurpose Minimal Solution",
+        }
+    });
+</script>
+
+<template>
+    <p>{{subTitle}}</p>
+    <h2>{{title}}</h2>
+</template>
