47e97ea5620493353d5c5dfef2ad311d89da5463
= <=>
2025-03-14 15:16:12 +0500

    feat: slide-11

---
+++ components/slide-11.vue
@@ -0,0 +1,72 @@
+<script setup>
+
+import SlideLayout from "@/components/ui/slideLayout.vue";
+</script>
+
+<template>
+    <slide-layout html-class="slide-11">
+        <img src="@/assets/imgs/slide-11.jpg" alt="">
+        <div class="slide-11__content">
+            <div class="slide-11__item">
+                <h3>Project 01</h3>
+                <p>Rapidiously harness mission process
+                    and backward communities.</p>
+            </div>
+            <div class="slide-11__item">
+                <h3>Project 02</h3>
+                <p>Rapidiously harness mission process
+                    and backward communities.</p>
+            </div>
+            <div class="slide-11__item">
+                <h3>Project 02</h3>
+                <p>Rapidiously harness mission process
+                    and backward communities.</p>
+            </div>
+        </div>
+    </slide-layout>
+</template>
+
+<style>
+
+    .slide-11 {
+        position: relative;
+    }
+
+    .slide-11__container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        gap: 20px;
+    }
+
+    .slide-11__container::after {
+        content: "";
+        width: 35%;
+        height: 100vh;
+
+        background: rgba(196, 196, 196, 0.3);
+
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: -1;
+    }
+
+    .slide-11__container > img {
+        max-width: 720px;
+    }
+
+    .slide-11__content {
+        max-width: 470px;
+        width: 100%;
+    }
+
+    .slide-11__item:not(:first-child) {
+        margin-top: 60px;
+    }
+
+    .slide-11__item p {
+        margin-top: 32px;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,7 @@
     import Slide8 from "@/components/slide-8.vue";
     import Slide9 from "@/components/slide-9.vue";
     import Slide10 from "@/components/slide-10.vue";
+    import Slide11 from "@/components/slide-11.vue";
 
     const siteModules = [
         {name: "hero", component: Hero},
@@ -22,6 +23,7 @@
         {name: "slide-8", component: Slide8},
         {name: "slide-9", component: Slide9},
         {name: "slide-10", component: Slide10},
+        {name: "slide-11", component: Slide11},
     ]
 </script>
 
